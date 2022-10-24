bf642d55a6e1cfb678b635734dbdc1334ea271bc
tomedinabren <tomedina@bren.ucsb.edu>
2022-10-24 11:30:07 -0700

    edited economics

--- economics.R
+++ economics.R
@@ -4,11 +4,20 @@
 
 library(tidyverse)
 
+# Function 1: integrate(y = a*x^b) from 0 to x
+# This is a general function format where a and b are constants and x is the flow rate
+# y = 
+# The function is integrated to get the total cost
+cost_function <- function(a, b, x){
+  
+  int <- integrate(function(y)
+    {a * y ^ b}, lower = 0, upper = x)
+  return(int$value)
+  
+}
 
-# William's Power Logarithmic Rule
-# log(y) = a(log(x))^b + c
 
-calculate_costs <- function(a, b, c, x, year, units){
+calculate_costs <- function(a, b, c, x, year){
   
   costs <- data.frame()
   
@@ -16,31 +25,27 @@ calculate_costs <- function(a, b, c, x, year, units){
     # iterates through every input
       if (c[i] != 0){
         
-        x <- x * 3785.4
+        x <- x * 3785.4 # convert MGD to m3/d
+        
+        # William's Power Logarithmic Rule
+        # log(y) = a(log(x))^b + c
         y <- a[i] * log(x) ^ (b[i]) + c[i] # calculates log(y)
         final_y <- 10 ^ y 
         costs <- rbind(costs, final_y) # binds the output for each iteration to a df
         
       } else {
         
-        int <- integrate(function(y)
-          {a[i] * y ^ b[i]}, lower = 0, upper = x)
-        
-        y_value <- int$value
-        
-        if (year[i] == 'euro_2014') {
-          y_dollar <- 1.1723 * y_value # 2020 Euro to current dollar (2022 October)
-          costs <- rbind(costs, y_dollar)
-        }
-        
-        else if (year[i] == 2014) {
+        if (year[i]== 2014) {
+          y_value <- cost_function(a[i], b[i], x)
           y_conversion <- 1.25 * y_value # converts from 2014 dollars to current dollar (2022 October)
           costs <- rbind(costs, y_conversion)
         }
         
         else {
-          y_conversion <- 1.15 * y_value # converts from 2020 dollars to current dollar (2022 October)
-          costs <- rbind(costs, y_conversion)
+          x <- x * 3785.4 # convert MGD to m3/d
+          y_value <- cost_function(a[i], b[i], x)
+          y_dollar <- 1.1723 * y_value # 2020 Euro to current dollar (2022 October)
+          costs <- rbind(costs, y_dollar)
         }
       
     }}
@@ -50,36 +55,6 @@ calculate_costs <- function(a, b, c, x, year, units){
   
 }
 
-
-omcost <- function(a, b, c, x, name){
-  
-  costs <- data.frame()
-  
-  for (i in 1:length(a)){
-    
-    if (!is.na(a[i])){
-      
-      y <- a[i] * log(x) ^ (b[i]) + c[i] # calculates log(y)
-      final_y <- 10 ^ y 
-      costs <- rbind(costs, final_y)
-      
-    } else {
-      
-      if (name[i] == 'uv disinfection'){
-        
-        y <- coefficients(uv_lm)[2] * x + coefficients(uv_lm)[1]
-        costs <- rbind(costs, y)
-        
-      } else {
-        
-        next
-        
-      }}}
-  
-  costs_sum <- sum(costs)
-  return(costs_sum)
-  
-}
 
 # From Hilbig et. al.
 # Create a df of necessary economic information (Guo et. al.)
@@ -93,8 +68,8 @@ coag <- data.frame('name' = 'coagulation & flocculation',
                    'oma' = 0.347,
                    'omb' = 1.448,
                    'omc' = 2.633,
-                   'year' = 'euro_2014',
-                   'yearom' = 'euro_2014')
+                   'year' = NA,
+                   'yearom' = NA)
 
 # reverse osmosis
 ro <- data.frame('name' = 'reverse osmosis',
@@ -104,8 +79,8 @@ ro <- data.frame('name' = 'reverse osmosis',
                  'oma' = 0.534,
                  'omb' = 1.253,
                  'omc' = 2.786,
-                 'year' = 'euro_2014',
-                 'yearom' = 'euro_2014')
+                 'year' = NA,
+                 'yearom' = NA)
 
 # ultrafiltration
 uf <- data.frame('name' = 'ultrafiltration',
@@ -115,8 +90,8 @@ uf <- data.frame('name' = 'ultrafiltration',
                  'oma' = 1.828,
                  'omb' = 0.598,
                  'omc' = 1.876,
-                 'year' = 'euro_2014',
-                 'yearom' = 'euro_2014')
+                 'year' = NA,
+                 'yearom' = NA)
 
 # granular activated carbon
 gac <- data.frame('name'= 'granular activated carbon',
@@ -126,19 +101,19 @@ gac <- data.frame('name'= 'granular activated carbon',
                   'oma' = 1.669,
                   'omb' = 0.559,
                   'omc' = 2.371,
-                  'year' = 'euro_2014',
-                  'yearom' = 'euro_2014')
+                  'year' = NA,
+                  'yearom' = NA)
 
 # chlorination (Capital Cost - Hilbig et. al. (2020))
 cl <- data.frame('name' = 'chlorination',
                  'a' = 3.416,
                  'b' = -0.422,
                  'c' = 0,
-                 'oma' = NA,
-                 'omb' = NA,
-                 'omc' = NA,
+                 'oma' = 0,
+                 'omb' = 0,
+                 'omc' = 0,
                  'year' = 2020,
-                 'yearom' = NA)
+                 'yearom' = 2014)
 
 # ozonation
 o3 <- data.frame('name' = 'ozonation',
@@ -158,7 +133,7 @@ uv <- data.frame('name' = 'uv disinfection + ozone',
                  'c' = 0,
                  'oma' = 0.016,
                  'omb' = -0.02,
-                 'omc' = NA,
+                 'omc' = 0,
                  'year' = 2014,
                  'yearom' = 2014)
 
@@ -168,7 +143,7 @@ uvh2o2 <- data.frame('name' = 'uv disinfection + h2o2',
                  'c' = 0,
                  'oma' = 0.038,
                  'omb' = -0.052,
-                 'omc' = NA,
+                 'omc' = 0,
                  'year' = 2014,
                  'yearom' = 2014)
 
@@ -178,15 +153,16 @@ mf <- data.frame('name' = 'microfiltration',
                  'c' = 0,
                  'oma' = 0.3,
                  'omb' = -0.22,
-                 'omc' = NA,
+                 'omc' = 0,
                  'year' = 2014,
                  'yearom' = 2014)
+
 
 # combines the dataframes together
 total <- rbind(coag, ro, uf, gac, cl, o3, uv, uvh2o2, mf)
 
 # tests the function using a flow rate of 10 MGD
-cctest <- calculate_costs(total$a, total$b, total$c, 10, )
-omtest <- omcost(total$oma, total$omb, total$omc, 10, total$name)
+cctest <- calculate_costs(total$a, total$b, total$c, 10, total$year)
+omtest <- calculate_costs(total$oma, total$omb, total$omc, 10, total$yearom)
 
 
